3491afb3fe80d29cf9cd8bc2a3ff80626190f95c
aprachov <andrii.prachov@gmail.com>
2023-07-13 14:04:58 +0200

    Add repository, domain, converter

---
+++ src/main/java/dataart/workshop/domain/AuthUserDetails.java
@@ -0,0 +1,52 @@
+package dataart.workshop.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class AuthUserDetails implements UserDetails {
+
+    private final User user;
+    private final Collection<? extends GrantedAuthority> grantedAuthorities;
+
+    public AuthUserDetails(User user) {
+        this.user = user;
+        this.grantedAuthorities = user.getRole().getAuthorities();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return grantedAuthorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
